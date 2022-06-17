f3a23add089c6bcb7eeddd0f6a76a57c26c05057
NGUYENDINHATK2 <dinhatnguyen81@gmail.com>
2022-06-17 17:19:09 +0700

    first commit

---
+++ src/screens/Map.js
@@ -0,0 +1,17 @@
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <View>
+            <Text>Map</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+})
